a242ae87d10ac155f8577b7f929a80c93ed10560
midrare <midrare9@gmail.com>
2025-02-07 13:38:38 -0800

    refactor: allow serial to be disabled

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -8,7 +8,6 @@
 #define PMW3389_SENSOR_NCS_PIN 5
 #include "PMW3389.h"
 
-#define SERIAL_ENABLED 1
 #define SERIAL_TIMEOUT 5000
 #define SERIAL_BAUD 9600
 
@@ -26,20 +25,22 @@ PMW3389 sensor;
 PMW3389_DATA sensorData = {};
 
 void setup() {
-    #if defined(SERIAL_ENABLED) && (SERIAL_ENABLED > 0)
-    Serial.begin(SERIAL_BAUD);
-    for (int i=0; i<SERIAL_TIMEOUT; i+=50) {
-      if (Serial) {
-        break;
-      }
-      delay(50);
+    if (SERIAL_BAUD >= 0) {
+        Serial.begin(SERIAL_BAUD);
+        if (SERIAL_TIMEOUT < 0) {
+            while (!Serial) {
+                delay(50);
+            }
+        } else {
+            for (int i=0; i<SERIAL_TIMEOUT && !Serial; i+=50) {
+              delay(50);
+            }
+        }
     }
-    Serial.println("Initializing...");
-    #endif
 
-    #if defined(SERIAL_ENABLED) && (SERIAL_ENABLED > 0)
+    Serial.println("Initializing...");
+
     Serial.println("Initializing button pins...");
-    #endif
     pinMode(MOUSE_LEFT_BUTTON_PIN, INPUT_PULLUP);
     pinMode(MOUSE_RIGHT_BUTTON_PIN, INPUT_PULLUP);
     pinMode(MOUSE_BACK_BUTTON_PIN, INPUT_PULLUP);
@@ -49,9 +50,7 @@ void setup() {
     pinMode(MOUSE_EXTRA2_BUTTON_PIN, INPUT_PULLUP);
 
 
-    #if defined(SERIAL_ENABLED) && (SERIAL_ENABLED > 0)
     Serial.println("Initializing sensor...");
-    #endif
     pinMode(PMW3389_SENSOR_NCS_PIN, OUTPUT);
     digitalWrite(PMW3389_SENSOR_NCS_PIN, HIGH);
     pinMode(PMW3389_SENSOR_RESET_PIN, OUTPUT);
@@ -59,28 +58,17 @@ void setup() {
     // signature check fails, but device works regardless
     sensor.begin(PMW3389_SENSOR_NCS_PIN);
 
-    #if defined(SERIAL_ENABLED) && (SERIAL_ENABLED > 0)
     Serial.println("Initializing mouse...");
-    #endif
     Trackball.begin();
     Trackball.setMoveScale(0.1, 0.1);
     Trackball.setScrollScale(0.01, 0.01);
-    #if defined(SERIAL_ENABLED) && (SERIAL_ENABLED > 0)
     Serial.println("Initialization done. Entering main loop...");
-    #endif
 }
 
 
 void loop() {
     sensorData = sensor.readBurst();
     if (sensorData.isOnSurface && sensorData.isMotion) {
-        #if defined(SERIAL_ENABLED) && (SERIAL_ENABLED > 0)
-        Serial.print("Motion: ");
-        Serial.print(sensorData.dx);
-        Serial.print(", ");
-        Serial.println(sensorData.dy);
-        #endif
-
         Trackball.move(sensorData.dx, sensorData.dy);
     }
 
